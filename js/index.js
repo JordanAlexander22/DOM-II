300cd193e6962b875e930c6b39336f0bf1b56898
jordan <jordan@jordans-Air.lan>
2019-10-29 21:35:02 +0700

    added variable list before actually making changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const firstButton = document.querySelector('.first');
+const MouseOverThing = document.querySelector('.intro h2')
+const keyStuff = document.querySelector('html')
+const doubleClick = document.querySelector('.intro h2')
+const mouseDown = document.querySelector('.intro h2')
+const mouseUp = document.querySelector('.intro h2')
+const mouseMove = document.querySelector('.intro h2')
+const mouseEnter = document.querySelector('.intro h2')
+const mouseLeave = document.querySelector('.intro h2')
+const keyUp = document.querySelector('.intro')
+
+// set up variables first, thought it would be cleaner.
